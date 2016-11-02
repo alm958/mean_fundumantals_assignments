d899b92bb61083cd98d0f14f677e8cc5c8f8190b
alm958 <alm958@gmail.com>
2016-11-02 13:52:38 -0400

    fixed Template literals syntax replacing quotes with back-ticks

--- JS_fund_II/JS_fund_II.js
+++ JS_fund_II/JS_fund_II.js
@@ -70,3 +70,70 @@ var MeanArray = function (arr){
 }
 var testMean = MeanArray(myArr2);
 console.log(testMean);
+
+var SomeObject = {
+    SumORange : function (x,y){
+        var sum = 0;
+        for (var i = x; i <= y; i++){
+            sum += i;
+        }
+        return sum;
+    },//end SumORange
+    GetArrMin : function (arr){
+        min = arr[0];
+        for (var i = 1; i < arr.length; i++){
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    },//end GetArrMin
+    GetArrMean : function (arr){
+        var sum = 0;
+        for (var i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        var mean = sum / arr.length;
+        return mean;
+    }//end GetArrMean
+}
+
+var test2minarr = SomeObject.GetArrMin(myArr);
+console.log(test2minarr);
+var test2meanarr = SomeObject.GetArrMean(myArr2);
+console.log(test2meanarr);
+var test2sumarr = SomeObject.SumORange(0,3);
+console.log(test2sumarr);
+
+var person = {
+    name : 'Tony',
+    distance_traveled : 0,
+    say_name : function(){
+        console.log(person.name);
+        return person;
+    },
+    say_something : function(string){
+        console.log(`${person.name} says ${string}`);
+        return person;
+    },
+    walk : function(){
+        console.log(`${person.name} is walking.`);
+        person.distance_traveled += 3;
+        return person;
+    },
+    run : function(){
+        console.log(`${person.name} is running.`);
+        person.distance_traveled += 10;
+        return person;
+    },
+    crawl : function(){
+        console.log(`${person.name} is crawling.`);
+        person.distance_traveled += 1;
+        return person;
+    }
+}
+
+person.walk().run().crawl();
+
+console.log(person.distance_traveled);
+console.log(person);
